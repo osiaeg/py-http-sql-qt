e9c3060bc92eb5f24acbc1310edcb51702e3ec5e
osiaeg <egor.goncharov.osia@mail.ru>
2023-06-15 21:26:11 +0300

    Set template

---
+++ client/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='http_client',
+    version='1.0.0',
+    author='R&EC SPb ETU',
+    author_email='info@nicetu.spb.ru',
+    url='http://nicetu.spb.ru',
+    description='GUI-клиент редактирования БД',
+    long_description="",
+    zip_safe=False,
+    packages=['http_client'],
+)
